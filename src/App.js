9486cb68e383f0529a4b512deb72eb7662dbde7b
Elena Petrova <yongminni97@gmail.com>
2019-12-10 04:34:12 +0300

    page layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import Header from "./components/Header";
+import SideBar from './components/SideBar'
+import Main from './components/Main';
+import {Container, Row, Col} from 'react-bootstrap'
+import './styles/bootstrap-custom.sass';
 
 function App() {
   return (
-    <></>
+    <Container>
+      <Header/>
+      <Row>
+          <Col md={3}>
+              <SideBar/>
+          </Col>
+          <Col md={9}>
+              <Main/>
+          </Col>
+      </Row>
+    </Container>
   );
 }
 
